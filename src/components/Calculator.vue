86b86b3c6e46f898f6674ea314a0820946a8fbae
Sander Johansen <sander.olin@gmail.com>
2023-01-23 23:20:25 +0100

    Finished prototype design

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="grid-container">
-        <div class="calculator-row">
+        <div class="calculator-row" id="outer-calculator-div">
             <div id="calculator-output-div">
                 <div id="calculator-output">
                     5400
@@ -8,31 +8,41 @@
             </div>
         </div>
         <div class="calculator-row">
-            <button class="calculator-button" v-for="n in calculatorFirstRow" :key="n">
+            <button class="calculator-button" v-for="n in calculatorFirstRow" :key="n"
+                :class="{'pink-buttons': ['PWR','DEL','AC','÷'].includes(n)}">
                 {{ n }}
             </button>
         </div>
         
         <div class="calculator-row">
-            <button class="calculator-button" v-for="n in calculatorSecondRow" :key="n">
+            <button class="calculator-button" v-for="n in calculatorSecondRow" :key="n"
+                :class="{'green-buttons': ['7','8','9'].includes(n), 'pink-buttons': ['x'].includes(n)}">
                 {{ n }}
             </button>
         </div>
 
         <div class="calculator-row">
-            <button class="calculator-button" v-for="n in calculatorThirdRow" :key="n">
+            <button class="calculator-button" v-for="n in calculatorThirdRow" :key="n"
+                :class="{'green-buttons': ['4','5','6'].includes(n), 'pink-buttons': ['-'].includes(n)}">
                 {{ n }}
             </button>
         </div>
 
         <div class="calculator-row">
-            <button class="calculator-button" v-for="n in calculatorFourthRow" :key="n">
+            <button class="calculator-button" v-for="n in calculatorFourthRow" :key="n"
+                :class="{'green-buttons': ['1','2','3'].includes(n), 'pink-buttons': ['+'].includes(n)}">
                 {{ n }}
             </button>
         </div>
 
         <div class="calculator-row">
-            <button class="calculator-button" v-for="n in calculatorFifthRow" :key="n">
+            <div id="zero-button-div">
+                <button class="calculator-button, green-buttons" id="zero-button">
+                    0
+                </button>
+            </div>
+            <button class="calculator-button" v-for="n in calculatorFifthRow" :key="n"
+                :class="{'pink-buttons': ['.','='].includes(n)}">
                 {{ n }}
             </button>
         </div>
@@ -45,11 +55,11 @@
 
         data() {
             return {
-                calculatorFirstRow: ["DEL", "AC", "÷"],
+                calculatorFirstRow: ["PWR","DEL", "AC", "÷"],
                 calculatorSecondRow: ["7", "8", "9", "x"],
                 calculatorThirdRow: ["4", "5", "6", "-"],
                 calculatorFourthRow: ["1", "2", "3", "+"],
-                calculatorFifthRow: ["0", ".", "="]
+                calculatorFifthRow: [".", "="]
             }
         }
     }
@@ -77,36 +87,67 @@
     padding: 0;
     margin: 0;
     width: 500px;
-    min-height: 100px;
-    text-align: right;
+    min-height: 88px;
+    text-align: left;
+    display: flex;
 }
 
 .calculator-button{
     padding: 0;
     margin: 0;
     min-width: 125px;
-    height: 70px;
+    height: 88px;
     font-size: 40px;
-    border-radius: 0;
+    border-radius: 5px;
 }
 
 #calculator-output{
     padding: 0;
     margin: 0;
+    padding-top: 12px;
+    padding-right: 10px;
+    height: 60px;
     text-align: right;
     justify-items: center;
-    font-size: 50px;
+    font-size: 70px;
     font-family: 'Orbitron', sans-serif;
     
 }
 
 #calculator-output-div{
     width: 460px;
-    height: 80px;
+    height: 100px;
+    border-radius: 20px;
     margin-left: 20px;
-    margin-top: 12px;
+    margin-top: 25px;
     background-color: rgb(6, 6, 50);
 }
 
+#outer-calculator-div{
+    height: 160px;
+}
+
+.pink-buttons{
+    background-color: #fe67c2;
+    border-color: #fe67c2;
+}
+
+.green-buttons{
+    background-color: #62f2b4;
+    border-color: #62f2b4
+}
+
+#zero-button-div{
+    display: grid;
+    width: 250px;
+    min-height: 88px;
+    margin: 0;
+    padding: 0;
+}
+
+#zero-button{
+    font-size: 40px;
+    border-radius: 5px;
+}
 
 </style>
