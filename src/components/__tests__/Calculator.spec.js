90280703ccd295704b13f87e26f5e8dfb64e5497
Sander Johansen <sander.olin@gmail.com>
2023-02-15 00:02:06 +0100

    Implemented Unit testing for the Calculator component with Vitest

---
+++ src/components/__tests__/Calculator.spec.js
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest"
+import { mount } from "@vue/test-utils"
+import Calculator from '../Calculator.vue'
+
+describe("Calculator", () => {
+    const wrapper = mount(Calculator)
+    it("Should render properly", () => {
+        expect(wrapper.html()).toMatchSnapshot
+    })
+    it("Should contain zero button", () => {
+        expect(wrapper.findComponent('calculator-button').isVisible()).toBe(true)
+    })
+})
